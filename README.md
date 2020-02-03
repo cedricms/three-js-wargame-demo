621886a04a254c3d1067cfbad7cd6b34463f1d9d
Cédric Maria-Sube <cedric@maria-sube.com>
2020-02-03 15:21:34 +0100

    Adds support for Browser Sync.

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ A quick demo to show how to implement a wargame with three.js.
 
 ## Build the project
 `npm run build`
+
+## Run the project with BrowserSync support
+`npm start`
+
+Connect to :
+
+[http://localhost:3000/](http://localhost:3000/)
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,21 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'dist')
     },
     plugins: [
         new CopyPlugin([
             { from: './*.html', to: '.' },
         ]),
+        new BrowserSyncPlugin({
+            host: 'localhost',
+            port: 3000,
+            proxy: 'http://localhost:8080/'
+        })
     ]
 };
