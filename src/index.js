de9c1b21854b5fa701fea7be58f29a587b939d30
Cédric Maria-Sube <cedric@maria-sube.com>
2020-02-03 14:42:13 +0100

    Adds three.js dependencies.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import _ from 'lodash';
+import * as THREE from 'three';
 
 function component() {
     const element = document.createElement('div');
+
+    const scene = new THREE.Scene();
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
