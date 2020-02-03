8bdae891a2798d345c0b63dcc2300dabddd2346c
Cédric Maria-Sube <cedric@maria-sube.com>
2020-02-03 14:35:13 +0100

    Sets up the project for Webpack.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # three-js-wargame-demo
 A quick demo to show how to implement a wargame with three.js.
+
+## Build the project
+`npm run build`
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new CopyPlugin([
+            { from: './*.html', to: '.' },
+        ]),
+    ]
+};
